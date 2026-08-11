33f74983f4c2f6babceffaba7e3954c1ed53a036
MZA73 <zazouamohammed@gmail.com>
2026-08-11 19:59:49 +0200

    Charge la signature MO (/sign-mo.png) dans window.ATRIUM_SIGN_PNG au montage

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,7 +11,9 @@ import "@fontsource/cormorant-garamond/500.css";
 import "@fontsource/cormorant-garamond/600.css";
 import "@fontsource/cormorant-garamond/500-italic.css";
 import "../globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+useEffect(() => { fetch("/sign-mo.png").then((r) => r.blob()).then((b) => { const fr = new FileReader(); fr.onload = () => { window.ATRIUM_SIGN_PNG = fr.result; }; fr.readAsDataURL(b); }).catch(() => {}); }, []);
   return <Component {...pageProps} />;
 }
